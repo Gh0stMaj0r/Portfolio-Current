fb558498f9414b509cff2fd06af3dc932c63d4eb
Gh0stMaj0r <suhonen.arttu2002@gmail.com>
2024-01-18 10:06:41 +0200

    Designs

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,9 @@
+//https://www.figma.com/community/file/1118248748041249031
+//https://www.figma.com/community/file/1191026033275812161
+//https://www.behance.net/gallery/176332383/Hi-Dev-Web-Developer-portfolio-landing-page?tracking_source=search_projects&l=11
+//https://www.behance.net/gallery/157781767/Design-a-cool-portfolio-for-a-web-developer?tracking_source=search_projects&l=2
+//
+
 import React from 'react';
 import './App.css';
 
